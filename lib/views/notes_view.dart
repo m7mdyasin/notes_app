14a726edbbdba64b19405c5caf7f931a5d44393e
mohammedyasin021 <160766863+mohammedyasin021@users.noreply.github.com>
2025-09-08 23:23:27 +0300

    create fetch notes method

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/custom_floating_action_buttoun.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
@@ -8,27 +10,30 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: CustomFloatingActionButtoun(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-              isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24),
-              ),
-              context: context,
-              builder: (context) {
-                return AddNoteBottomSheet();
-              },
-            );
-          },
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          child: Icon(Icons.add, color: Color(0xFF3a0ca3), size: 32),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: CustomFloatingActionButtoun(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                context: context,
+                builder: (context) {
+                  return AddNoteBottomSheet();
+                },
+              );
+            },
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            child: Icon(Icons.add, color: Color(0xFF3a0ca3), size: 32),
+          ),
         ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
