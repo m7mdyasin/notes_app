a19f7eb2a2fa1d3af66ab9a29b3aeb1c11e04860
mohammedyasin021 <160766863+mohammedyasin021@users.noreply.github.com>
2025-09-08 22:34:44 +0300

    refactor multi bloc provider

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -8,7 +8,8 @@ class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
       child: BlocConsumer<AddNoteCubit, AddNoteState>(
         listener: (context, state) {
           if (state is AddNoteFailure) {
@@ -21,7 +22,7 @@ class AddNoteBottomSheet extends StatelessWidget {
         builder: (context, state) {
           return ModalProgressHUD(
             inAsyncCall: state is AddNoteLoading ? true : false,
-            child: AddNoteForm(),
+            child: const SingleChildScrollView(child: AddNoteForm()),
           );
         },
       ),
--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custoum_text_field.dart';
 
@@ -40,6 +43,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
               onPressed: () {
                 if (formKey.currentState!.validate()) {
                   formKey.currentState!.save();
+                  var noteModel = NoteModel(
+                    date: DateTime.now().toString(),
+                    subtitle: description!,
+                    title: title!,
+                  );
+                  BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+
                   print('title $title');
                   print('description $description');
                 } else {
