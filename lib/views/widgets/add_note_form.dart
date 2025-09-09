579e29f368d57723a6f1133ab3b2036f0ad72958
mohammedyasin021 <160766863+mohammedyasin021@users.noreply.github.com>
2025-09-09 06:35:07 +0300

    format date time

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
@@ -46,8 +47,10 @@ class _AddNoteFormState extends State<AddNoteForm> {
                   onPressed: () {
                     if (formKey.currentState!.validate()) {
                       formKey.currentState!.save();
+                      var currentDate = DateTime.now();
+                      var formattedDate = DateFormat.yMd().format(currentDate);
                       var noteModel = NoteModel(
-                        date: DateTime.now().toString(),
+                        date: formattedDate,
                         content: description!,
                         title: title!,
                       );
--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
-import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/note_card.dart';
 
 class NotesListView extends StatelessWidget {
